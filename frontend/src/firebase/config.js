28e18c489906b13abf6ff6b236051dfddb31fbf7
Lemagiciendelombre <booba8033@gmail.com>
2026-05-12 14:02:46 +0000

    Confirguration de Firebase

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,21 +1,26 @@
 // Configuration Firebase — remplace les valeurs par tes vraies credentials Firebase
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey:            "REMPLACE_PAR_TON_API_KEY",
-  authDomain:        "REMPLACE_PAR_TON_AUTH_DOMAIN",
-  projectId:         "REMPLACE_PAR_TON_PROJECT_ID",
-  storageBucket:     "REMPLACE_PAR_TON_STORAGE_BUCKET",
-  messagingSenderId: "REMPLACE_PAR_TON_MESSAGING_SENDER_ID",
-  appId:             "REMPLACE_PAR_TON_APP_ID",
+  apiKey: "AIzaSyBUVMhx3hOy66bIcHONsZrRqwbV_5lwCO0",
+  authDomain: "hirrde-pass.firebaseapp.com",
+  projectId: "hirrde-pass",
+  storageBucket: "hirrde-pass.firebasestorage.app",
+  messagingSenderId: "597549685708",
+  appId: "1:597549685708:web:417047a3e3b46c40f9db69",
+  measurementId: "G-8QRSTVKP0J"
 };
 
 const app     = initializeApp(firebaseConfig);
 const db      = getFirestore(app);
 const auth    = getAuth(app);
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
 
 export { app, db, auth, storage };
